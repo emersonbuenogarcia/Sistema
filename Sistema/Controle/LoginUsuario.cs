4780e99075ae30f6d901b7bf33015ae497dfec4d
emersonbuenogarcia <75962396+emersonbuenogarcia@users.noreply.github.com>
2020-12-21 00:36:55 -0300

    Add files via upload

---
+++ Sistema/Controle/LoginUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema.Modelo;
+
+namespace Sistema.Controle {
+    class LoginUsuario {
+        public bool Logar(usuario u)
+        {
+            try
+            {
+                using (bdlojaEntities2 ctx = new bdlojaEntities2())
+                {
+                    ctx.usuarios.Find(u);
+                }
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+    }
+}
