d6d5d005231ce3728b747b77da8ab98d8359642d
emersonbuenogarcia <75962396+emersonbuenogarcia@users.noreply.github.com>
2020-12-21 00:36:25 -0300

    Add files via upload

--- Sistema/Visao/Login.xaml.cs
+++ Sistema/Visao/Login.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Sistema.Controle;
+using Sistema.Modelo;
 
 namespace Sistema.Visao {
     /// <summary>
@@ -24,12 +26,33 @@ namespace Sistema.Visao {
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Logar();
+        }
 
+        private void Logar()
+        {
+            try
+            {
+                usuario u = new usuario();
+                u.usu_nome = txbUsuario.Text;
+                u.usu_senha = hashbytes('md5', psbSenha.Password);
+                if (LoginUsuario.Logar(u) == true)
+                {
+                    Principal principal = new Principal();
+                    principal.IsEnabled = true;
+                    principal.Show();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
